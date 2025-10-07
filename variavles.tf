6615d75c3df66825031ef2cdfe1c1b6c2c7fddff
anshuvgthakur <anshuvgthakur@gmail.com>
2025-10-07 18:53:09 +0530

    Create variavles.tf

---
+++ variavles.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+  type = string
+  default = "ami-04f25a69b566c844b"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "name_tag" {
+  type = string
+  default = "my-server"
+}
+
+
+
